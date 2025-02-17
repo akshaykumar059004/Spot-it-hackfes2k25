d581e9f129da264bf59f50ced5a425ddeb0c99d5
Akshay <akshaykumar059004@gmail.com>
2025-02-17 14:02:58 +0530

    Main activity

---
+++ app/src/main/java/com/developersunit/spot_it_hackfes2k25/MainActivity.java
@@ -0,0 +1,16 @@
+package com.developersunit.spot_it_hackfes2k25;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activitymain);
+
+        //setContentView(textView);
+    }
+}
