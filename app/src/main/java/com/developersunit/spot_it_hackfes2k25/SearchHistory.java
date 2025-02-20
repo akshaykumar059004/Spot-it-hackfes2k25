8a5bd34906fefe100396e5606d037053d6ac53bb
Akshay <akshaykumar059004@gmail.com>
2025-02-21 04:16:34 +0530

    Initial History Implementation

---
+++ app/src/main/java/com/developersunit/spot_it_hackfes2k25/SearchHistory.java
@@ -0,0 +1,13 @@
+package com.developersunit.spot_it_hackfes2k25;
+
+public class SearchHistory {
+    public int id;
+    public String query;
+    public String date;
+
+    public SearchHistory(int id, String query, String date) {
+        this.id = id;
+        this.query = query;
+        this.date = date;
+    }
+}
