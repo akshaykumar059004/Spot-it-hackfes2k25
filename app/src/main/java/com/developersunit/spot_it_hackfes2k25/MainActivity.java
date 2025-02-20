56abee0a4d6f6428a56bb346096ab72c4f04de1d
Akshay <akshaykumar059004@gmail.com>
2025-02-20 18:35:25 +0530

    Converted to fragment, Removed actionbar

--- app/src/main/java/com/developersunit/spot_it_hackfes2k25/MainActivity.java
+++ app/src/main/java/com/developersunit/spot_it_hackfes2k25/MainActivity.java
@@ -1,31 +1,32 @@
 package com.developersunit.spot_it_hackfes2k25;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.window.SplashScreen;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
-public class MainActivity extends Activity{
+public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activitymain);
-
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
             if (item.getItemId() == R.id.nav_home) {
-                startActivity(new Intent(MainActivity.this, MainActivity.class));
-                return true;
+                selectedFragment = new HomeFragment();
             } else if (item.getItemId() == R.id.nav_history) {
-                startActivity(new Intent(MainActivity.this, History.class));
-                return true;
+                selectedFragment = new HistoryFragment();
             } else if (item.getItemId() == R.id.nav_settings) {
-                startActivity(new Intent(MainActivity.this, Settings.class));
-                return true;
+                selectedFragment = new SettingsFragment();
             }
-            return false;
+            if(selectedFragment != null){
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            }
+            return true;
         });
     }
 }
